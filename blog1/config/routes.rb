a6f1ddad01e38c92dff88a99aafe6e2cbba0b0db
Salomanuel <salomanuel.aux+code@gmail.com>
2017-01-24 12:20:51 +0100

    achieve hello rails

--- blog1/config/routes.rb
+++ blog1/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get 'welcome/index'
+
+  get 'welcome/index'
+
+  get 'welcome/index'
+
   # get 'welcome/index'
   # root 'welcome#index'
 
