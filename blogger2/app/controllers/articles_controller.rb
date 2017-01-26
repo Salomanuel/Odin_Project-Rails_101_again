2f395bd895fa08b9572a9cbd4d08fa9d9024c0f3
Salomanuel <salomanuel.aux+code@gmail.com>
2017-01-26 22:01:55 +0100

    temp commit, editing has an issue, trying to merge

--- blogger2/app/controllers/articles_controller.rb
+++ blogger2/app/controllers/articles_controller.rb
@@ -18,4 +18,10 @@ class ArticlesController < ApplicationController
 		@article.save
 		redirect_to articles_path
 	end
+
+	def update
+	  @article = Article.find(params[:id])
+		@article.update(article_params)
+		redirect_to article_path(@article)
+	end
 end
