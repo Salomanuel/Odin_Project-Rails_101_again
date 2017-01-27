6e5f9a58f5f6671926b80fa0b3840cc64073cbbe
Salomanuel <salomanuel.aux+code@gmail.com>
2017-01-27 18:22:03 +0100

    add tags in a shady way I haven't quite understood

--- blogger2/app/models/article.rb
+++ blogger2/app/models/article.rb
@@ -3,6 +3,11 @@ class Article < ActiveRecord::Base
 	has_many :taggings
 	has_many :tags, through: :taggings
 
+	def tag_list=(tags_string)
+		tag_names = tags_string.split(",").collect{ |s| s.strip.downcase }.uniq
+		new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+	end
+
 	def tag_list
 		self.tags.collect { |tag|	tag.name }.join(", ")
 	end
